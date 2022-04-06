0bb41b9ee7d599b73f3eb02b32f581a89b171260
BuildTools <unconfigured@null.spigotmc.org>
2022-04-06 04:52:11 +0100

    initlal REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,55 @@
+const asyncHandler = require("express-async-handler");
+
+const Goals = require("../models/goalModel");
+
 //@desc     Get Goals
 //@route    GET /api/goals
 //@access   Private
-const getGoals = (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-};
+const getGoals = asyncHandler(async (req, res) => {
+  const goal = await Goals.find();
+
+  res.status(200).json(goal);
+});
 //@desc     Set Goal
 //@route    POST /api/goals
 //@access   Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
+  const goal = await Goals.create({ text: req.body.text });
 
-  res.status(200).json({ message: "Set Goal" });
-};
+  res.status(200).json(goal);
+});
 //@desc     Update Goals
 //@route    PUT /api/goals/:id
 //@access   Private
-const updateGoal = (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
-};
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goals.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  const updatedGoal = await Goals.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
+});
 //@desc     Delete Goals
 //@route    DELETE /api/goals/:id
 //@access   Private
-const deleteGoal = (req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` });
-};
+const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goals.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  const deltedGoal = await Goals.findByIdAndDelete(req.params.id);
+  res.status(200).json(deltedGoal);
+});
 
 module.exports = {
   getGoals,
